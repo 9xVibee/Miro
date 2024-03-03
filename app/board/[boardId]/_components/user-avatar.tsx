885b83b0158881cfea3a9106e9e3a70403e45a09
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-03 13:58:34 +0530

    feat: Pariticipants component completed

---
+++ app/board/[boardId]/_components/user-avatar.tsx
@@ -0,0 +1,29 @@
+import Hint from "@/components/hint";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface UserAvatarProps {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  borderColor?: string;
+}
+
+const UserAvatar = ({ borderColor, fallback, name, src }: UserAvatarProps) => {
+  return (
+    <Hint label={name || "Teammate"} sideOffset={18} side="bottom">
+      <Avatar
+        className="h-10 w-10 border-2"
+        style={{
+          borderColor,
+        }}
+      >
+        <AvatarImage src={src} />
+        <AvatarFallback className="text-xs font-semibold">
+          {fallback}
+        </AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
+};
+
+export default UserAvatar;
