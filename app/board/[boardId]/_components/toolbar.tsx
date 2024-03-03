3aeb44d10c13c8bbf5f08281d6b9d464a28194b6
Abhay Panchal <abpanchal951357@gmail.com>
2024-03-03 14:13:03 +0530

    chore: Undo Redo icons added

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,18 +1,71 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
+import ToolButton from "./tool-button";
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
 
 const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>pencil</div>
-        <div>square</div>
-        <div>circle</div>
-        <div>Ellipsis</div>
+        <ToolButton
+          icon={MousePointer2}
+          label="Select"
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          icon={Type}
+          label="Text"
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          icon={StickyNote}
+          label="Sticky Note"
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          icon={Square}
+          label="Rectangle"
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          icon={Circle}
+          label="Ellipse"
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          icon={Pencil}
+          label="Pen"
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>undo</div>
-        <div>redo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          isDisabled={true}
+          onClick={() => {}}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          isDisabled={true}
+          onClick={() => {}}
+        />
       </div>
     </div>
   );
